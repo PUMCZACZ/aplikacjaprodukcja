66fd08801d565bd3dd1cbdf22a366947686b6bd0
Gacek <szymon.gackowski@gmail.com>
2023-03-01 17:31:11 +0100

    2023-03-01_17-31-11

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -7,14 +7,19 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 /**
+ * @property int           id
  * @property OrderTypeEnum order_type
- * @property int client_id
- * @property int quantity
- * @property int price
- * @property bool is_completed
- * @property Carbon updated_at
- * @property Carbon created_at
- * @property Carbon deadline
+ * @property int           client_id
+ * @property int           quantity
+ * @property int           weight
+ * @property int           price
+ * @property bool          is_completed
+ * @property Carbon        updated_at
+ * @property Carbon        created_at
+ * @property Carbon        deadline
+ * @property int           admin_id
+ *
+ * @property float         packing_price
  */
 class Order extends Model
 {
