0153ca0fdca831836fdbe5bb289bb1c607b74f28
PUMCZACZ <pawel.cebernik02@gmail.com>
2023-03-07 21:59:49 +0100

    Add new display of time in orders section

--- resources/views/order/edit.blade.php
+++ resources/views/order/edit.blade.php
@@ -1,7 +1,7 @@
 <x-nav.layout>
     <section class="py-8 max-w-4xl mx-auto">
         <h1 class="text-lg font-bold mb-8 pb-2 border-b">
-            Edycja Zamówienia
+            Szczegóły Zamówienia: {{ $order->clients->name . ' '. $order->clients->lastname }}
         </h1>
         <div class="flex">
             <main class="flex-1">
@@ -15,7 +15,7 @@
                                     <option value="{{ $order->client_id }}"
                                             {{ $order->client_id == $order->clients->id ? 'selected' : '' }}
                                     >
-                                        {{ $order->clients->name . ' '. $order->clients->lastname}}
+                                        {{ $order->clients->name . ' '. $order->clients->lastname }}
                                     </option>
                             </select>
                         </x-form.field>
@@ -32,6 +32,12 @@
                         <x-form.input name="quantity" :value="old('quantity', $order->quantity)" required>Ilość</x-form.input>
                         <x-form.input name="price" type="numeric" :value="old('price', $order->priceToDolars())" required>Cena</x-form.input>
                         <x-form.input name="deadline" type="datetime-local" :value="old('deadline', $order->deadline)" required>Termin Realizacji Zamówienia</x-form.input>
+                            <div class="grid justify-items-end mt-6">
+                                <label class="text-sm">Ostatnia modyfikacja zamówienia</label>
+                                <p class="text-sm">{{ \Carbon\Carbon::parse($order->updated_at)->locale('pl')->diffForHumans() }}</p>
+                            </div>
+
+
                         <x-form.button>Dodaj</x-form.button>
                     </form>
                 </div>
