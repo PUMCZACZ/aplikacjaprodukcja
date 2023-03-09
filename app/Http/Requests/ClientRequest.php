be7286f2e3e4d5c05e32e8fb332cd12dffc475ff
PUMCZACZ <pawel.cebernik02@gmail.com>
2023-03-09 19:24:26 +0100

    Add reg_replace and custom error messages

--- app/Http/Requests/ClientRequest.php
+++ app/Http/Requests/ClientRequest.php
@@ -17,21 +17,23 @@ class ClientRequest extends FormRequest
             'lastname'     => ['min:3'],
             'city'         => ['required'],
             'status'       => ['min:1'],
-            'phone_number' => ['min:9'],
+            'phone_number' => ['min:11']
         ];
     }
 
     public function messages(): array
     {
         return [
+            'name.min' => 'Imię powinno się składać minimum z 3 znaków',
+            'lastname.min' => 'Nazwisko powinno się składać minimum z 3 znaków',
+            'city.requried' => 'To pole jest wymagane',
+            'phone_number.min' => 'numer telefonu powinien składać się z 9 cyfr oddzielanych znakiem "-" lub spacją',
         ];
     }
 
     public function formatPhoneNumber(): int
     {
-        $toReplace = ['-', '.', ' '];
-
-        return str_replace($toReplace, '', $this->input('phone_number'));
+        return preg_replace('/[^0-9]/', '', $this->input('phone_number'));
     }
 
     public function toData(): array
