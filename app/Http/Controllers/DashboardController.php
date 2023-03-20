42c360f42c3d0ec88cf52ab6d376f679dd3c6685
Gacek <szymon.gackowski@gmail.com>
2023-03-20 16:33:42 +0100

    policy examples

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -1,25 +1,23 @@
 <?php
 namespace App\Http\Controllers;
 
+use App\Models\Client;
+use App\Models\Order;
+use App\Models\Transport;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
 
 class DashboardController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('can:admin');
-    }
-
     public function index()
     {
-//        if (Auth::user()->can('admin')) {
-//            dd('admin');
-//        }
-//
-//        if (Gate::check('admin')) {
-//            dd('działam');
-//        }
+        $order = Order::first();
+
+        if (Auth::user()->can('super', $order)) {
+            dd('wszystko ok');
+        } else {
+            dd('nie ma uprawnień');
+        }
 
         return view('dashboard.index');
     }
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,7 +2,9 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+use App\Models\Order;
 use App\Models\User;
+use App\Policies\OrderPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
@@ -14,7 +16,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array<class-string, class-string>
      */
     protected $policies = [
-        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+        Order::class => OrderPolicy::class,
     ];
 
     /**
